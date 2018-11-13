dcca3f6d6f05fce9693564d3ccd5d7d5356a2156
cisco-cx-cdi <ciscocdi100@gmail.com>
2018-11-13 14:50:26 -0800

    ADD website pages generated, attempt to fix base url config

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -18,9 +18,4 @@ var store = [{
         "excerpt":"Conference Date: 11/2/2018-11/3/2018 Key Conference Takeaways “Software 2.0” AI, and in particular deep learning methods, is very powerful. AI models can be viewed as another way to ‘write’ a program. Instead of using procedural code, you simply give the program the desired input/output combinations, and it learns the proper program...","categories": ["Conference"],
         "tags": ["conference, active learning, bayesian optimization, bayesian statistics packages, continuous experimentation"],
         "url": "http://localhost:4000/conference/open-data-science-conference/",
-        "teaser":null},{
-        "title": "Calibration Curves: How Confident Are Your Confidence Scores?",
-        "excerpt":"## What is Prediction Confidence? ‘ML Engine’ is am service that google supports that allows a developer to run both ‘training’ and ‘serving’ jobs using google’s resources for ML models. ML Engine is a service that can be called for on-the-spot training of a tensorflow, XGBoost, or now (Think it’s...","categories": ["Best_Practices"],
-        "tags": ["Machine_Learning, Calibration_Curve"],
-        "url": "http://localhost:4000/best_practices/calibration-curves-ml/",
         "teaser":null}]
